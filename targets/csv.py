3c45c7d47c78192381b6164c8561e4495d4e0150
Eoghan Murray <eoghan@getthere.ie>
2017-08-09 10:59:25 +0100

    Use multiprocessing to speed up transformations - csv on RJFA from 280s down to 195s

--- targets/csv.py
+++ targets/csv.py
@@ -3,6 +3,8 @@ import csv as csv_module
 import logging
 import json
 import os
+from multiprocessing import Pool
+from time import time as t_time
 
 from lib.util import json_comment_filter
 from lib.fixed_fields import iterate_fixed_fields
@@ -11,12 +13,15 @@ from targets.unzip import unzip
 
 def csv(file_prefixes=None):
     log = logging.getLogger('targets_csv')
+    stime = t_time()
     csv_dir = os.path.join(os.getcwd(), 'feeds', 'csv')
     if not os.path.exists(csv_dir):
         os.makedirs(csv_dir)
     file_fields = json_comment_filter(json.load(open('file-fields.json', 'r')))
     if not file_prefixes:
         file_prefixes = file_fields.keys()
+
+    todo = []
     for fprefix in sorted(file_prefixes):
         extracted_dir = os.path.join(os.getcwd(), 'feeds', fprefix)
         if not os.path.isdir(extracted_dir):
@@ -33,26 +38,58 @@ def csv(file_prefixes=None):
                 log.warning('%s: Missing spec for %s' % (fprefix, filename))
                 continue
             log.debug('%s: Found spec for %s' % (fprefix, filename))
-            csv_files = {}
-            csv_writers = {}
-            try:
-                fields = file_fields[fprefix][filename]
-                for record in iterate_fixed_fields(os.path.join(extracted_dir, filename), fields):
-                    k = record.get('RECORD_TYPE', '')
-                    if k not in csv_writers:
-                        fsuffix = ''
-                        if k != '':
-                            fsuffix = '-' + k
-                        csv_path = os.path.join(os.getcwd(), 'feeds', 'csv',
-                                                fprefix + '-' + filename + fsuffix + '.csv')
-                        csv_files[k] = open(csv_path, 'w')
-                        csv_writers[k] = csv_module.DictWriter(
-                            csv_files[k],
-                            fieldnames=[f[0] for f in fields[k]])
-                        csv_writers[k].writeheader()
-                    if k:
-                        del record['RECORD_TYPE']  # contained in filename
-                    csv_writers[k].writerow(record)
-            finally:
-                for f in csv_files.values():
-                    f.close()
+            file_path = os.path.join(extracted_dir, filename)
+            if False:
+                # don't multiprocess
+                file_to_csv(fprefix, filename, file_path, file_fields)
+            else:
+                todo.append((fprefix, filename, file_path, file_fields))
+    if todo:
+        n = 1
+        with Pool() as pool:
+            for fprefix, filename, csv_count in pool.imap_unordered(file_to_csv_tup, todo):
+                csv_msg = ''
+                if csv_count > 1:
+                    csv_msg = '- %d csv files' % (csv_count)
+                log.info('Finished processing %s/%s (%d of %d) %s' % (
+                    fprefix, filename, n, len(todo), csv_msg))
+                n += 1
+    log.debug('csv: %ds total time' % (t_time()-stime))
+
+
+def file_to_csv_tup(tup):
+    # required as we don't have pool.starmap_unordered
+    return file_to_csv(*tup)
+
+
+def file_to_csv(fprefix, filename, file_path=None, file_fields=None):
+    """
+    A file can have multiple record types. Output separate
+    CSV files for each one
+    """
+    if file_path is None:
+        file_path = os.path.join(os.getcwd(), 'feeds', fprefix, filename)
+    csv_files = {}
+    csv_writers = {}
+    try:
+        fields = file_fields[fprefix][filename]
+        for record in iterate_fixed_fields(file_path, fields):
+            k = record.get('RECORD_TYPE', '')
+            if k not in csv_writers:
+                fsuffix = ''
+                if k != '':
+                    fsuffix = '-' + k
+                csv_path = os.path.join(os.getcwd(), 'feeds', 'csv',
+                                        fprefix + '-' + filename + fsuffix + '.csv')
+                csv_files[k] = open(csv_path, 'w')
+                csv_writers[k] = csv_module.DictWriter(
+                    csv_files[k],
+                    fieldnames=[f[0] for f in fields[k]])
+                csv_writers[k].writeheader()
+            if k:
+                del record['RECORD_TYPE']  # contained in filename
+            csv_writers[k].writerow(record)
+    finally:
+        for f in csv_files.values():
+            f.close()
+    return fprefix, filename, len(csv_files)
--- targets/unzip.py
+++ targets/unzip.py
@@ -4,15 +4,19 @@ import logging
 import shutil
 from zipfile import ZipFile
 from collections import defaultdict
+from multiprocessing import Pool
+from time import time as t_time
 
 from lib.util import json_comment_filter
 
 
 def unzip(file_prefixes=None):
     log = logging.getLogger('targets_unzip')
+    stime = t_time()
     if not file_prefixes:
         file_fields = json_comment_filter(json.load(open('file-fields.json', 'r')))
         file_prefixes = file_fields.keys()
+    todo = []
     for ftname in file_prefixes:
         if ftname.lower().endswith('.zip'):
             zipfilename = ftname
@@ -20,46 +24,64 @@ def unzip(file_prefixes=None):
         else:
             zipfilename = ftname + '-FULL-LATEST.ZIP'
         zpath = os.path.join(os.getcwd(), 'feeds', zipfilename)
-        zz = ZipFile(open(zpath, 'rb'))
-
-        feed_path = os.path.join(os.getcwd(), 'feeds', ftname)
-        if not os.path.exists(feed_path):
-            os.makedirs(feed_path)
-        versions = defaultdict(int)
-        extract_count = 0
-        for member in zz.infolist():
-            out_name = member.filename
-            if out_name.startswith(ftname):
-                out_name = out_name[len(ftname):]
-                if out_name[0] == 'F':
-                    out_name = out_name[1:]
-                if out_name[0] == 'C':
-                    log.error("Unexpected 'Changes' file in archive: %s" % (out_name))
-                    continue
-                if '.' in out_name and out_name.split('.')[0].isnumeric():
-                    version = out_name.split('.')[0]
-                    versions[version] += 1
-                    out_name = out_name.split('.', 1)[1]
-            zz.extract(member, feed_path)
-            if out_name != member.filename:
-                shutil.move(
-                    os.path.join(feed_path, member.filename),
-                    os.path.join(feed_path, out_name))
-            extract_count += 1
-        version_file = os.path.join(feed_path, '.version')
-        if not versions:
-            log.warning('No versioning found')
-            if os.path.exists(version_file):
-                os.path.remove(version_file)
+        if False:
+            # don't multiprocess
+            unzip_single(zpath, ftname)
         else:
-            most_common_version = max(versions.items(), key=lambda kv: kv[1])[0]
-            highest_version = max(versions.items())[0]
-            if most_common_version != highest_version:
-                log.warning('%s: Most common version (%d) is not highest (%d)'
-                            % (ftname, most_common_version, highest_version))
-            elif len(versions) > 1:
-                log.warning('%s: Multiple versions in single ZIP: %s'
-                            % (', '.join(map(int, sorted(versions)))))
-            with open(version_file, 'w') as vf:
-                vf.write(highest_version + '\n')
-        log.info('%s Extracted %d files to %s' % (ftname, extract_count, feed_path))
+            todo.append((zpath, ftname))
+    if todo:
+        with Pool() as pool:
+            for _ in pool.imap_unordered(unzip_single_tup, todo):
+                pass
+    log.debug('unzip: %d total time' % (t_time()-stime))
+
+
+def unzip_single_tup(tup):
+    # required as we don't have pool.starmap_unordered
+    return unzip_single(*tup)
+
+
+def unzip_single(zpath, ftname):
+    log = logging.getLogger('targets_unzip')
+    zz = ZipFile(open(zpath, 'rb'))
+    feed_path = os.path.join(os.getcwd(), 'feeds', ftname)
+    if not os.path.exists(feed_path):
+        os.makedirs(feed_path)
+    versions = defaultdict(int)
+    extract_count = 0
+    for member in zz.infolist():
+        out_name = member.filename
+        if out_name.startswith(ftname):
+            out_name = out_name[len(ftname):]
+            if out_name[0] == 'F':
+                out_name = out_name[1:]
+            if out_name[0] == 'C':
+                log.error("Unexpected 'Changes' file in archive: %s" % (out_name))
+                continue
+            if '.' in out_name and out_name.split('.')[0].isnumeric():
+                version = out_name.split('.')[0]
+                versions[version] += 1
+                out_name = out_name.split('.', 1)[1]
+        zz.extract(member, feed_path)
+        if out_name != member.filename:
+            shutil.move(
+                os.path.join(feed_path, member.filename),
+                os.path.join(feed_path, out_name))
+        extract_count += 1
+    version_file = os.path.join(feed_path, '.version')
+    if not versions:
+        log.warning('No versioning found')
+        if os.path.exists(version_file):
+            os.path.remove(version_file)
+    else:
+        most_common_version = max(versions.items(), key=lambda kv: kv[1])[0]
+        highest_version = max(versions.items())[0]
+        if most_common_version != highest_version:
+            log.warning('%s: Most common version (%d) is not highest (%d)'
+                        % (ftname, most_common_version, highest_version))
+        elif len(versions) > 1:
+            log.warning('%s: Multiple versions in single ZIP: %s'
+                        % (', '.join(map(int, sorted(versions)))))
+        with open(version_file, 'w') as vf:
+            vf.write(highest_version + '\n')
+    log.info('%s Extracted %d files to %s' % (ftname, extract_count, feed_path))
