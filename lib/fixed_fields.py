b043a4ea5c26a92105e45ba8eb1b20cd6d3e7996
Eoghan Murray <eoghan@getthere.ie>
2017-08-09 10:08:15 +0100

    More debugging context for which feed the file belongs to

--- lib/fixed_fields.py
+++ lib/fixed_fields.py
@@ -12,10 +12,10 @@ def field_sum(field_pairs):
 
 def iterate_fixed_fields(file_path, fields, RECORD_TYPE=None, full_only=True):
     log = logging.getLogger('iterate_fixed_fields')
-    file_name = file_path.split('/')[-1]
-    log.debug('ingesting %s' % (file_name))
+    file_sig = '/'.join(file_path.split('/')[-2:])
+    log.debug('ingesting %s' % (file_sig))
     if not fields or not sum(fields.values(), []):
-        log.warning('No fields defined for %s' % (file_name))
+        log.warning('No fields defined for %s' % (file_sig))
         return
     with open(file_path, 'r') as f:
         last_fi = 0
@@ -34,12 +34,12 @@ def iterate_fixed_fields(file_path, fields, RECORD_TYPE=None, full_only=True):
                         len(line) == sum(field_lens) + 1):
                     if full_only and line[0] != 'R':
                         raise Exception('%s Expected full file, not '
-                                        'changes Line "%s"' % (file_name, line))
+                                        'changes Line "%s"' % (file_sig, line))
                     offset = 1
             else:
                 if len(set(map(len, fields.keys()))) > 1:
                     raise Exception('%s Need to deal with mixed size RECORD_TYPE'
-                                    'field lengths in the same file' % (file_name))
+                                    'field lengths in the same file' % (file_sig))
                 fkl = len(max(fields.keys()))
                 if (line[0] in UPDATE_MARKER_vals and
                         line[1:][:fkl] in fields and
@@ -48,7 +48,7 @@ def iterate_fixed_fields(file_path, fields, RECORD_TYPE=None, full_only=True):
                     offset = 1 + fkl
                     if full_only and line[0] != 'R':
                         raise Exception('%s Expected full file, not'
-                                        'changes Line "%s"' % (file_name, line))
+                                        'changes Line "%s"' % (file_sig, line))
                 elif (line[:fkl] in fields and
                         len(line) == field_sum(fields[line[:fkl]]) + fkl):
                     record_type = line[:fkl]
@@ -56,17 +56,17 @@ def iterate_fixed_fields(file_path, fields, RECORD_TYPE=None, full_only=True):
                 elif (line[0] in UPDATE_MARKER_vals and
                         line[1:][:fkl] in fields):
                     raise Exception('%s Line "%s" (len %d) doesn\'t match spec (len %d): %s %r' % (
-                        file_name, line, len(line), field_sum(fields[line[1:][:fkl]]),
+                        file_sig, line, len(line), field_sum(fields[line[1:][:fkl]]),
                         line[1:][:fkl], fields[line[1:][:fkl]]))
                 else:
                     raise Exception('%s Can\'t find a record type for line "%s" (len %d)' %
-                                    (file_name, line, len(line)))
+                                    (file_sig, line, len(line)))
                 field_names = [f[0] for f in fields[record_type]]
                 field_lens = [f[1] for f in fields[record_type]]
                 ld['RECORD_TYPE'] = record_type
             if sum(field_lens) != len(line)-offset:
                 raise Exception('%s Line "%s" (len %d) doesn\'t match spec (len %d): %s %r' % (
-                    file_name, line, len(line), sum(field_lens), record_type,
+                    file_sig, line, len(line), sum(field_lens), record_type,
                     fields[record_type]))
             for i, l in enumerate(field_lens):
                 fstart = sum(field_lens[:i]) + offset
@@ -85,7 +85,7 @@ def iterate_fixed_fields(file_path, fields, RECORD_TYPE=None, full_only=True):
                         per_sec = '%d' % (lines_per_sec)
                     else:
                         per_sec = '%.2f' % (lines_per_sec)
-                    log.debug('%s %s lines per second' % (file_name, per_sec))
+                    log.debug('%s %s lines per second' % (file_sig, per_sec))
                     last_fi = fi
                     stime = t_time()
                     last_rtime = rtime
